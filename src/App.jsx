38daab99c775f7fb165b947b3391ddf9112fc60d
Maximiliano Sanchez <makzofx@gmail.com>
2023-03-28 18:34:58 -0300

    un poco mas avanzado. articles v1

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ const App = () => {
       <header className="container mx-auto px-4 pt-8">
 
         <div className="flex justify-between">
-        <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">Todo</h1>
-        <button>Luna</button>
+          <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">Todo</h1>
+          <button>Luna</button>
         </div>
 
         <form className="bg-white rounded-md overflow-hidden px-4 flex gap-4 items-center mt-8">
@@ -19,27 +19,28 @@ const App = () => {
       </header>
 
       <main className="container mx-auto px-4 mt-8">
-       
-       <div className="bg-white rounded-md px-4">
-       <article>
-          <button className="rounded-full border-2 w-5 h-5 inline-block"></button>
-          <p>Complete online JS Curse makzz</p>
-          <button> <CrossIcon/> </button>
-        </article>
 
-        <article>
-        <button className="rounded-full border-2 w-5 h-5 inline-block"></button>
-          <p className="text-gray-600 ">Complete online JS Curse makzz</p>
-          <button> <CrossIcon/> </button>
-        </article>
+        <div className="bg-white rounded-md">
 
-        <article>
-        <button className="rounded-full border-2 w-5 h-5 inline-block"></button>
-          <p>Complete online JS Curse makzz</p>
-          <button> <CrossIcon/> </button>
-        </article>
+          <article className="flex gap-4 py-4 border-b-gray-800 border-b  px-4">
+            <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+            <p className="grow">Complete online JS Curse makzz</p>
+            <button className="flex-none"> <CrossIcon /> </button>
+          </article>
 
-       </div>
+          <article className="flex gap-4 py-4 border-b-gray-800 border-b  px-4">
+            <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+            <p className="grow">Complete online JS Curse makzz</p>
+            <button className="flex-none"> <CrossIcon /> </button>
+          </article>
+
+          <article className="flex gap-4 py-4 border-b-gray-800 border-b  px-4">
+            <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+            <p className="grow">Complete online JS Curse makzz</p>
+            <button className="flex-none"> <CrossIcon /> </button>
+          </article>
+
+        </div>
 
 
         <section>
