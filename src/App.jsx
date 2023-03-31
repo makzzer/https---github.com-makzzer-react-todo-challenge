5d342a88bc432167b9c22a779306426cab41ed70
Makz Sánchez <makzofx@gmail.com>
2023-03-31 08:57:48 -0300

    luchando con svg

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ const App = () => {
           <h1 className="text-3xl font-semibold uppercase tracking-[0.3em] text-white">
             Todo
           </h1>
-          <button><MoonIcon/></button>
+          <button>
+            <MoonIcon className="fill-red-700" />
+          </button>
         </div>
 
         <form className="mt-8 flex items-center gap-4 overflow-hidden rounded-md bg-white px-4">
@@ -59,15 +61,15 @@ const App = () => {
         </div>
       </main>
 
-      <section className="container mx-auto px-4 mt-8">
-        <div className="bg-white p-4 rounded flex justify-center gap-4">
+      <section className="container mx-auto mt-8 px-4">
+        <div className="flex justify-center gap-4 rounded bg-white p-4">
           <button className="text-blue-600">All</button>
           <button className="hover:text-blue-600">Active</button>
           <button className="hover:text-blue-600">Completed</button>
         </div>
       </section>
 
-      <section className=" container mx-auto px-4 text-center mt-8">
+      <section className=" container mx-auto mt-8 px-4 text-center">
         Drag and drop to reorder list
       </section>
     </div>
