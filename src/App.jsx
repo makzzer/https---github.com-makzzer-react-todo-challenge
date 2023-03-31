7832f7157851e1db142619692aef983588d8533d
Makz Sánchez <makzofx@gmail.com>
2023-03-31 08:17:01 -0300

    cambios en estructura acomodando sin componentizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,77 @@
-import CrossIcon from "./components/icons/CrossIcon"
+import CrossIcon from "./components/icons/CrossIcon";
+import MoonIcon from "./components/icons/MoonIcon";
 
 const App = () => {
   return (
     //el fragment es porque no podemos tener elementos sueltos sino que tenemos que devolver un unico elemento en el componente
-    <div className="bg-[url('./images/bg-mobile-light.jpg')] bg-no-repeat bg-contain bg-gray-300 min-h-screen">
-
+    <div className="min-h-screen bg-gray-300 bg-[url('./images/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
       <header className="container mx-auto px-4 pt-8">
-
         <div className="flex justify-between">
-          <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">Todo</h1>
-          <button>Luna</button>
+          <h1 className="text-3xl font-semibold uppercase tracking-[0.3em] text-white">
+            Todo
+          </h1>
+          <button><MoonIcon/></button>
         </div>
 
-        <form className="bg-white rounded-md overflow-hidden px-4 flex gap-4 items-center mt-8">
-          <span className="rounded-full border-2 w-5 h-5 inline-block"></span>
-          <input className="px-4 py-4 w-full text-gray-400 outline-none" type="text" placeholder="Crear una nueva tarea..." />
+        <form className="mt-8 flex items-center gap-4 overflow-hidden rounded-md bg-white px-4">
+          <span className="inline-block h-5 w-5 rounded-full border-2"></span>
+          <input
+            className="w-full px-4 py-4 text-gray-400 outline-none"
+            type="text"
+            placeholder="Crear una nueva tarea..."
+          />
         </form>
       </header>
 
-      <main className="container mx-auto px-4 mt-8">
-
-        <div className="bg-white rounded-md [&>article]:p-4">
-
-          <article className="flex gap-4 border-b-gray-800 border-b">
-            <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+      <main className="container mx-auto mt-8 px-4">
+        <div className="rounded-md bg-white [&>article]:p-4">
+          <article className="flex gap-4 border-b border-b-gray-800">
+            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
             <p className="grow">Complete online JS Curse makzz</p>
-            <button className="flex-none"> <CrossIcon /> </button>
+            <button className="flex-none">
+              {" "}
+              <CrossIcon />{" "}
+            </button>
           </article>
 
-          <article className="flex gap-4  border-b-gray-800 border-b">
-            <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+          <article className="flex gap-4  border-b border-b-gray-800">
+            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
             <p className="grow">Complete online JS Curse makzz</p>
-            <button className="flex-none"> <CrossIcon /> </button>
+            <button className="flex-none">
+              {" "}
+              <CrossIcon />{" "}
+            </button>
           </article>
 
-          <article className="flex gap-4 border-b-gray-800 border-b">
-            <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+          <article className="flex gap-4 border-b border-b-gray-800">
+            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
             <p className="grow">Complete online JS Curse makzz</p>
-            <button className="flex-none"> <CrossIcon /> </button>
+            <button className="flex-none">
+              {" "}
+              <CrossIcon />{" "}
+            </button>
           </article>
 
-
-          <section className="py-4 px-4 justify-between flex">
+          <section className="flex justify-between py-4 px-4">
             <span className="text-gray-400">5 items left</span>
             <button className="text-gray-400">Clear Completed</button>
           </section>
-
         </div>
       </main>
 
-      <section className="container mx-auto px-4">
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+      <section className="container mx-auto px-4 mt-8">
+        <div className="bg-white p-4 rounded flex justify-center gap-4">
+          <button className="text-blue-600">All</button>
+          <button className="hover:text-blue-600">Active</button>
+          <button className="hover:text-blue-600">Completed</button>
+        </div>
       </section>
 
-      <section className=" text-center container mx-auto px-4">
+      <section className=" container mx-auto px-4 text-center mt-8">
         Drag and drop to reorder list
       </section>
-
     </div>
-
-  )
-}
+  );
+};
 
 export default App;
