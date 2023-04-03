abc0f99033b59d17de515b90555575d89e713a1e
Makz Sánchez <makzofx@gmail.com>
2023-04-03 03:37:23 -0300

    factorizacion del main en futuros compnentes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import MoonIcon from "./components/icons/MoonIcon";
 const App = () => {
   return (
     //el fragment es porque no podemos tener elementos sueltos sino que tenemos que devolver un unico elemento en el componente
-    <div className="min-h-screen bg-gray-300 bg-[url('./images/bg-mobile-light.jpg')] bg-cover bg-no-repeat">
+   
+   
+   <div className="min-h-screen bg-gray-300 bg-[url('./images/bg-mobile-light.jpg')] bg-cover bg-no-repeat">
+   
+   
+   {/*Header (Componente) en vuejs recomiendan qe los componentes no sean de 1 palabra, en react si se podría*/}
       <header className="container mx-auto px-4 pt-8">
         <div className="flex justify-between">
           <h1 className="text-3xl font-semibold uppercase tracking-[0.3em] text-white">
@@ -14,8 +19,11 @@ const App = () => {
             <MoonIcon className="fill-green-400" />
           </button>
         </div>
+      </header>
 
-        <form className="mt-8 flex items-center gap-4 overflow-hidden rounded-md bg-white px-4">
+      <main className="container mx-auto mt-8 px-4">
+        {/* TODO Create */}
+        <form className="flex items-center gap-4 overflow-hidden rounded-md bg-white px-4">
           <span className="inline-block h-5 w-5 rounded-full border-2"></span>
           <input
             className="w-full px-4 py-4 text-gray-400 outline-none"
@@ -23,10 +31,9 @@ const App = () => {
             placeholder="Crear una nueva tarea..."
           />
         </form>
-      </header>
 
-      <main className="container mx-auto mt-8 px-4">
-        <div className="rounded-md bg-white [&>article]:p-4">
+        {/* TODO LIST (TodoItem) TODOUpdate y TODODelete */}
+        <div className="rounded-t-md bg-white [&>article]:p-4 mt-8">
           <article className="flex gap-4 border-b border-b-gray-800">
             <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
             <p className="grow">Complete online JS Curse makzz</p>
@@ -53,25 +60,27 @@ const App = () => {
               <CrossIcon />{" "}
             </button>
           </article>
-
-          <section className="flex justify-between py-4 px-4">
-            <span className="text-gray-400">5 items left</span>
-            <button className="text-gray-400">Clear Completed</button>
-          </section>
         </div>
+
+        {/*TODO Computed-->Osea operaciones como contar los items restantes*/}
+        <section className="flex justify-between py-4 px-4 bg-white rounded-b-md">
+          <span className="text-gray-400">5 items left</span>
+          <button className="text-gray-400">Clear Completed</button>
+        </section>
+
+        {/*TODO FILTER */}
+        <section className="container mx-auto mt-8">
+          <div className="flex justify-center gap-4 rounded bg-white p-4">
+            <button className="text-blue-600">All</button>
+            <button className="hover:text-blue-600">Active</button>
+            <button className="hover:text-blue-600">Completed</button>
+          </div>
+        </section>
       </main>
 
-      <section className="container mx-auto mt-8 px-4">
-        <div className="flex justify-center gap-4 rounded bg-white p-4">
-          <button className="text-blue-600">All</button>
-          <button className="hover:text-blue-600">Active</button>
-          <button className="hover:text-blue-600">Completed</button>
-        </div>
-      </section>
-
-      <section className=" container mx-auto mt-8 px-4 text-center">
+      <footer className=" container mx-auto mt-8 px-4 text-center">
         Drag and drop to reorder list
-      </section>
+      </footer>
     </div>
   );
 };
