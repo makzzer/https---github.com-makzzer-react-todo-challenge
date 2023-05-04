95e8da640c9415da5755a466fbb760947bb8df22
Makz Sánchez <makzofx@gmail.com>
2023-05-04 18:13:22 -0300

    cambiado nombre imagenes bg netflify

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const listaTodosDefault = [
 const initialStateTodos = JSON.parse(localStorage.getItem('todos')) || listaTodosDefault;
 
 const App = () => {
-  const [todos, setTodos] = useState(initialStateTodos);
+  const [todos, setTodos] = useState(listaTodosDefault);
 
   //Creo otro state para los filtros
   const [filtro, setFiltro] = useState("all");
@@ -117,7 +117,7 @@ const App = () => {
   return (
     //el fragment es porque no podemos tener elementos sueltos sino que tenemos que devolver un unico elemento en el componente
 
-    <div className="min-h-screen bg-gray-300 bg-[url('./images/bg-mobile-light.jpg')] bg-cover    bg-no-repeat dark:bg-gray-800 dark:bg-[url('./images/bg-mobile-dark.jpg')] dark:text-gray-400">
+    <div className="min-h-screen bg-gray-300 bg-[url('./images/bgMobileLight.jpg')] bg-cover    bg-no-repeat dark:bg-gray-800 dark:bg-[url('./images/bgMobileDark.jpg')] dark:text-gray-400">
       <Header />
 
       <main className="container mx-auto mt-8 px-4">
