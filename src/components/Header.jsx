7decab263e1d87a2e3a5e8a255f2ececac2160c9
Makz Sánchez <makzofx@gmail.com>
2023-05-04 15:35:08 -0300

    agregado switch darkmode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import IconMoon from "./icons/IconMoon";
 import IconSun from "./icons/IconSun";
 
 //{/*Header (Componente) en vuejs recomiendan qe los componentes no sean de 1 palabra, en react si se podría*/}
 const Header = () => {
-  //para renderizar el modo oscuro necesito un estado, por eso lo inicio por default en falso
+  //para renderizar el modo oscuro necesito un estado, por eso lo inicio por default en falso que significa que esa el modo light
   const [darkMode, setDarkMode] = useState(false);
+
+  //lo primero es el codigo que se va a ejecutar con el use effect 
+  //y entre [] va el parametro al que debe estar atento al cambio, 
+  //en este caso el cambio de true y false del boton luna y sol
+  useEffect(()=>{
+    darkMode ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark')
+  },[darkMode])
 
   return (
     <header className="container mx-auto px-4 pt-8">
