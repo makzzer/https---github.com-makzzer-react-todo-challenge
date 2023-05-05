1aeb77d084fa17134c85ccaddd194782815b9cdf
Maximiliano Sanchez <makzofx@gmail.com>
2023-05-05 02:12:43 -0300

    dark mode no persiste despues de F5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,37 @@ import { useEffect, useState } from "react";
 import IconMoon from "./icons/IconMoon";
 import IconSun from "./icons/IconSun";
 
+//acá voy a definir el estado inicial en lo que esté en localStorage, si viene dark lo dejo en dark por mas que presione F5,
+//esto es para que repete las preferencias de visualizacion del usuario aunque actualice el sitio web
+//el metodo ya devuelve un booleano
+const initialStateDarkMode = localStorage.getItem("theme") === 'dark' ? true : false;
+//console.log(localStorage.getItem('theme'))
+
 //{/*Header (Componente) en vuejs recomiendan qe los componentes no sean de 1 palabra, en react si se podría*/}
 const Header = () => {
   //para renderizar el modo oscuro necesito un estado, por eso lo inicio por default en falso que significa que esa el modo light
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(initialStateDarkMode);
 
   //lo primero es el codigo que se va a ejecutar con el use effect 
   //y entre [] va el parametro al que debe estar atento al cambio, 
   //en este caso el cambio de true y false del boton luna y sol
-  useEffect(()=>{
-    darkMode ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark')
-  },[darkMode])
+  useEffect(() => {
+    if (darkMode) {
+
+      document.documentElement.classList.add("dark")
+      //para persistir el tema segun la preferencia del usuario incluso cuando actualicé, guardo theme=light o theme=dark, en localStorage
+      localStorage.theme = 'dark'
+      console.log(localStorage.getItem('theme'))
+      
+    } else {
+      document.documentElement.classList.remove("dark");
+      //se puede hacer de esta otra manera tambien
+      localStorage.setItem('theme','light')
+      console.log(localStorage.getItem('theme'))
+    }
+    console.log(initialStateDarkMode)
+
+  }, [darkMode])
 
   return (
     <header className="container mx-auto px-4 pt-8">
